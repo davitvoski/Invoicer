a000989fffb555d59aa5c7ead19e21a8829c5a02
Davit_V <davit.voskerchyan@dawsoncollege.qc.ca>
2024-04-24 14:57:34 -0400

    Add mudblazor

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -3,26 +3,26 @@ using BlazorBootstrap;
 
 namespace Invoicer
 {
-    public static class MauiProgram
-    {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				});
 
-            builder.Services.AddMauiBlazorWebView();
-            builder.Services.AddBlazorBootstrap();
+			builder.Services.AddMauiBlazorWebView();
+			builder.Services.AddBlazorBootstrap();
 #if DEBUG
 			builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
+			builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
-        }
-    }
+			return builder.Build();
+		}
+	}
 }
